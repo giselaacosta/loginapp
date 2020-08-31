5743711a8ebd6d8495bb513986abcb5f457ffb0e
Gisela Acosta <gacosta@technisys.com>
2020-08-30 23:05:12 -0300

    c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCbkgAKcGN8xr1zWL2xNEzff3NNruri7_4",
+    authDomain: "login-ng-25a71.firebaseapp.com",
+    databaseURL: "https://login-ng-25a71.firebaseio.com",
+    projectId: "login-ng-25a71",
+    storageBucket: "login-ng-25a71.appspot.com",
+    messagingSenderId: "1057996606863",
+    appId: "1:1057996606863:web:6d69320e5a2e8c67320ffa"
+  },
 };
